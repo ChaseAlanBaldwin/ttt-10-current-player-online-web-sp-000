df8d657713750994a60a4e06908a73db1c8db833
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-09-14 02:12:00 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,13 @@
+
+
 def turn_count(board)
   i = 0
 
   board.each do |turns|
-    if board(turns) == 'X' || board(turns) == 'O'
+    if board[turns] == 'X' || board[turns] == 'O'
       i += 1
     end
   end
 
-  return i 
+  return i
 end
